8573013c097e4c1d73866edc1a5ff437923b9767
Yash <yashagr070403@gmail.com>
2022-07-05 06:33:23 +0530

    Added feature of update,edit and delete of camps

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -11,9 +13,12 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("DataBase Connected")
 });
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
@@ -22,7 +27,8 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
+    res.render('campgrounds/index', { campgrounds });
+
 });
 
 app.get('/campgrounds/new', (req, res) => {
@@ -30,7 +36,9 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', async (req, res) => {
-    res.send(req.body);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -38,6 +46,23 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground });
 });
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+});
+
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+
+})
 
 
 app.listen(3000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 40) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city},${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores deleniti a id, aliquid blanditiis placeat at consequatur soluta similique dolor, incidunt ab vel dolorem saepe maxime. Corrupti corporis atque quidem.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores deleniti a id, aliquid blanditiis placeat at consequatur soluta similique dolor, incidunt ab vel dolorem saepe maxime. Corrupti corporis atque quidem.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores deleniti a id, aliquid blanditiis placeat at consequatur soluta similique dolor, incidunt ab vel dolorem saepe maxime. Corrupti corporis atque quidem.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores deleniti a id, aliquid blanditiis placeat at consequatur soluta similique dolor, incidunt ab vel dolorem saepe maxime. Corrupti corporis atque quidem.',
+            price: price
         })
 
         await camp.save();
